c0b6a33f28a1da985b8a53951bd572461453de0f
Julio Camarena <julio0631armenta@gmail.com>
2025-09-01 14:47:52 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int Luffy[] = {1, 69, 31, 4, 5};
+    
+    std::cout<<"Arreglos C++";
+    
+    for(int i = 0; i < 5; i++){
+        cout << "\nEl numero de la posicion [ " << i << " ] Es: " << Luffy[i] ;
+    }
+    
+
+    return 0;
+}
